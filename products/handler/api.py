bbd47fbacef7b94098244ef97b02c825df091db1
Oniely <jabagatniel09@gmail.com>
2025-03-11 13:08:32 +0800

    s

--- products/handler/api.py
+++ products/handler/api.py
@@ -5,17 +5,19 @@ from model.product import Product
 import os
 
 
-PRODUCTS_TABLE = os.environ.get('PRODUCTS_TABLE')
+PRODUCTS_TABLE = os.environ.get("PRODUCTS_TABLE")
+
 
 class DecimalEncoder(json.JSONEncoder):
-  def default(self, obj):
-    if isinstance(obj, Decimal):
-      return str(obj)
-    return json.JSONEncoder.default(self, obj)
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
 
 def hello(event, context):
     body = {
-        "message": "We are team Oniely and this is the hello world from lambda",
+        "message": "We are team Oniely and this is the hello world from lambdas",
     }
 
     print("I added a pretty little print here for debugging")
@@ -24,161 +26,200 @@ def hello(event, context):
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
- 
+
+
 def get_all_products(event, context):
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
     table = dynamodb.Table(PRODUCTS_TABLE)
-    
+
     return_body = {}
-    return_body["items"] = table.scan().get('Items')
-    
+    return_body["items"] = table.scan().get("Items")
+
     return_body["status"] = "success"
-    response = {"statusCode": 200,"headers": {
-        "Content-Type": "application/json"
-    }, "body": json.dumps(return_body, cls=DecimalEncoder)}
-    
+    response = {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(return_body, cls=DecimalEncoder),
+    }
+
     return response
-    
-    
+
+
 def create_one_product(event, context):
     body = json.loads(event.get("body", "{}"), parse_float=Decimal)
-    product_id = body['product_id']
-    
+    product_id = body["product_id"]
+
     if not body:
-        return {"statusCode": 400, "body": json.dumps({"error": "Request body is required"})}
-        
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Request body is required"}),
+        }
+
     if not product_id:
-        return {'statusCode': 400, "body": json.dumps({'error': 'Required a Product ID'})}
-        
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Required a Product ID"}),
+        }
+
     product = Product()
-    
-    new_product_response = product.save(product_id, body['product_name'], body['brand_name'], body['price'], body['quantity'])
-    
+
+    new_product_response = product.save(
+        product_id,
+        body["product_name"],
+        body["brand_name"],
+        body["price"],
+        body["quantity"],
+    )
+
     if "error" in new_product_response:
-        return {"statusCode": 400, "body": json.dumps({'error': new_product_response['error']})}
-        
-    
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": new_product_response["error"]}),
+        }
+
     return_body = {}
-    return_body['items'] = body
-    
-    if 'message' in new_product_response:
-        return_body['message'] = new_product_response['message']
-    
+    return_body["items"] = body
+
+    if "message" in new_product_response:
+        return_body["message"] = new_product_response["message"]
+
     response = {"statusCode": 200, "body": json.dumps(return_body, cls=DecimalEncoder)}
-    
-    # sqs queue 
-    sqs = boto3.resource('sqs', region_name='us-east-2')
-    queue = sqs.get_queue_by_name(QueueName='my-sqs-oniely')    
+
+    # sqs queue
+    sqs = boto3.resource("sqs", region_name="us-east-2")
+    queue = sqs.get_queue_by_name(QueueName="my-sqs-oniely")
     response = queue.send_message(MessageBody=json.dumps(body, cls=DecimalEncoder))
-    
+
     print(return_body)
-    
+
     return {"statusCode": 200, "body": json.dumps(return_body, cls=DecimalEncoder)}
- 
+
+
 def get_product(event, context):
     path_params = event.get("pathParameters", {})
     product_id = path_params.get("id")
-    
+
     if not product_id:
-        return {"statusCode": 400, "body": json.dumps({"error": "Product ID is required"})}
-    
-    
-    
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Product ID is required"}),
+        }
+
     return_body = {}
     product = Product().get(product_id)
-    
+
     if not product:
         return {"statusCode": 404, "body": json.dumps({"error": "Product not found"})}
-    
-    return_body['items'] = product
-    response = {"statusCode": 200, "headers": {
-        "Content-Type": "application/json"
-    },"body": json.dumps(return_body, cls=DecimalEncoder)}
-    
+
+    return_body["items"] = product
+    response = {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(return_body, cls=DecimalEncoder),
+    }
+
     return response
-    
+
+
 def delete_product(event, context):
     path_params = event.get("pathParameters", {})
     product_id = path_params.get("id")
-    
+
     if not product_id:
-        return {"statusCode": 400, "body": json.dumps({"error": "Product ID is required"})}
-    
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Product ID is required"}),
+        }
+
     return_body = {}
     delete_product_response = Product().delete(product_id)
-    
+
     if not delete_product_response:
-        return {"statusCode": 404, "body": json.dumps({"error": "Product deletion failed."})}
-    
-    return_body['status'] = 'success'
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"error": "Product deletion failed."}),
+        }
+
+    return_body["status"] = "success"
     response = {"statusCode": 200, "body": json.dumps(return_body, cls=DecimalEncoder)}
-    
+
     return response
-    
-    
+
+
 def update_product(event, context):
     path_params = event.get("pathParameters", {})
     product_id = path_params.get("id")
-    
+
     if not product_id:
-        return {"statusCode": 400, "body": json.dumps({"error": "Product ID is required"})}
-    
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Product ID is required"}),
+        }
+
     body = json.loads(event.get("body", "{}"), parse_float=Decimal)
-    
+
     if not body:
-        return {"statusCode": 400, "body": json.dumps({"error": "Request body is required"})}
-    
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Request body is required"}),
+        }
+
+    dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
     table = dynamodb.Table(PRODUCTS_TABLE)
-    
+
     return_body = {}
-    data = table.get_item(Key={'product_id': product_id})
-    item = data.get('Item')
-    
+    data = table.get_item(Key={"product_id": product_id})
+    item = data.get("Item")
+
     if not item:
-        return {"statusCode": 404, "body": json.dumps({"message": "Product ID not found"})}
-        
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"message": "Product ID not found"}),
+        }
+
     update_expression = "SET " + ", ".join(f"{key} = :{key}" for key in body.keys())
     expression_values = {f":{key}": value for key, value in body.items()}
-    
+
     updated_product = Product().update(product_id, update_expression, expression_values)
-    
-    if 'error' in updated_product:
-        return {"statusCode": 400, "body": json.dumps({"error": updated_product['error']})}
-    
-    return_body['status'] = 'success'
-    response = {"statusCode": 200, "body": json.dumps(updated_product, cls=DecimalEncoder)}
-    
+
+    if "error" in updated_product:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": updated_product["error"]}),
+        }
+
+    return_body["status"] = "success"
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(updated_product, cls=DecimalEncoder),
+    }
+
     return response
-    
+
+
 def get_product_by_name(event, context):
     path_params = event.get("pathParameters", {})
     product_name = path_params.get("name")
-    
+
     print(product_name)
-    
+
     if not product_name:
-        return {"statusCode": 400, "body": json.dumps({"error": "Product name is required"})}
-    
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Product name is required"}),
+        }
+
     return_body = {}
     product = Product().get_by_name(product_name)
-    
+
     if not product:
         return {"statusCode": 404, "body": json.dumps({"error": "Product not found"})}
-    
-    return_body['items'] = product[0]
-    response = {"statusCode": 200, "headers": {
-        "Content-Type": "application/json"
-    },"body": json.dumps(return_body, cls=DecimalEncoder)}
-    
+
+    return_body["items"] = product[0]
+    response = {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(return_body, cls=DecimalEncoder),
+    }
+
     return response
-    
-def buy_product(event,context):
-    path_params = event.get("pathParameters", {})
-    product_id = path_params.get("id")
-    
-    if not product_id:
-        return {"statusCode": 400, "body": json.dumps({"error": "Product ID is required"})}
-    
-    return_body = {}
-    buy_product_response = Product().buy_product(product_id)
--- products/model/product.py
+++ products/model/product.py
@@ -3,33 +3,34 @@ from botocore.exceptions import ClientError
 import os
 
 
-PRODUCT_TABLE = os.environ.get('PRODUCT_TABLE')
+PRODUCTS_TABLE = os.environ.get("PRODUCTS_TABLE")
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(PRODUCT_TABLE)
+table = dynamodb.Table(PRODUCTS_TABLE)
+
 
 class Product:
     @staticmethod
     def save(product_id, product_name, brand_name, price, quantity):
-        """ Save product to DynamoDB """
+        """Save product to DynamoDB"""
         try:
             response = table.scan(
                 FilterExpression="product_id = :id OR product_name = :name",
-                ExpressionAttributeValues={":id": product_id, ":name": product_name}
+                ExpressionAttributeValues={":id": product_id, ":name": product_name},
             )
 
             existing_product = response.get("Items", [])
-            
+
             if existing_product:
                 existing_product = existing_product[0]
-            
+
                 if existing_product["product_id"] == product_id:
                     print(f"Product with ID {product_id} already exists.")
                     return {"error": "Product ID already exists."}
                 if existing_product["product_name"] == product_name:
                     print(f"Product with name '{product_name}' already exists.")
                     return {"error": "Product name already exists."}
-            
+
             table.put_item(
                 Item={
                     "product_id": product_id,
@@ -46,68 +47,55 @@ class Product:
 
     @staticmethod
     def get(product_id):
-        """ Retrieve a product by ID """
+        """Retrieve a product by ID"""
         try:
             response = table.get_item(Key={"product_id": product_id})
             return response.get("Item")
         except ClientError as e:
             print(f"Error retrieving product: {e}")
             return None
-            
+
     @staticmethod
     def get_by_name(product_name):
-        """ Retrieve a product by ID """
+        """Retrieve a product by ID"""
         try:
             response = table.scan(
                 FilterExpression="product_name = :name",
-                ExpressionAttributeValues={":name": product_name}
+                ExpressionAttributeValues={":name": product_name},
             )
-            
-            return response.get('Items', [])
+
+            return response.get("Items", [])
         except ClientError as e:
             print(f"Error retrieving product: {e}")
             return None
-    
+
     @staticmethod
     def delete(product_id):
-        """ Delete a product """
+        """Delete a product"""
         try:
             table.delete_item(Key={"product_id": product_id})
             return True
         except ClientError as e:
             print(f"Error deleting product: {e}")
             return False
-        
+
     @staticmethod
     def update(product_id, update_expression, expression_value):
-        """ Update a product """
+        """Update a product"""
         try:
             existing_product = Product.get(product_id)
-            
+
             if not existing_product:
                 return {"error": "Product doesn't exist."}
-                
+
             response = table.update_item(
                 Key={"product_id": product_id},
                 UpdateExpression=update_expression,
                 ExpressionAttributeValues=expression_value,
-                ReturnValues="ALL_NEW"
+                ReturnValues="ALL_NEW",
             )
-            
-            return response.get('Attributes')
+
+            return response.get("Attributes")
         except ClientError as e:
             print(f"Error updating product: {e}")
             return {"error": e}
-       
-    @staticmethod     
-    def buy_product(product_id):
-        try:
-            existing_product = Product.get(product_id)
-            
-            if not existing_product:
-                return {"error": "Product doesn't exist."}
-                
-            return ""
-        except ClientError as e:
-            print(f'Error buting product: {e}')
-            return {'error': e}
